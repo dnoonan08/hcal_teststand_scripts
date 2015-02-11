cca75d7e80911cea39626c92f232edbd752e4cfd
Elliot Hughes <tote@physics.rutgers.edu>
2015-02-11 18:19:25 +0100

    150211: I made the active_channels and check_cid functions more reliable. I also made a few minor changes.

--- qie_card_valid.py
+++ qie_card_valid.py
@@ -4,6 +4,7 @@ import os, re
 n_bx = 7
 n_samples = n_bx*4-4
 n_channel = 14
+ip_uhtr = "192.168.29.40"		# The IP address of the uHTR you want to contact
 
 # Read data from the uHTR
 uhtr_script = '''## BEGIN uhtr script ##
@@ -27,7 +28,7 @@ with open("qie_card_valid.txt", "w") as out:
 #print "---"
 #print uhtr_script
 #print "---"
-raw_output = "{0}\n{1}".format("=========", os.popen("uHTRtool.exe 192.168.29.40 -s qie_card_valid.txt").read())
+raw_output = "{0}\n{1}".format("=========", os.popen("uHTRtool.exe {0} -s qie_card_valid.txt".format(ip_uhtr)).read())
 #raw_output = open("qie_card_valid_out.txt").read()
 with open("qie_card_valid_out.txt", "w") as out:
 	out.write(raw_output)
@@ -37,17 +38,20 @@ def active_channels(raw):		# Produces a list of activated channels
 	active = []
 	n_times = 0
 	for line in raw.split("\n"):
-		if re.search("^Status\s*\|", line):
+		if re.search("^BadCounter(\s*(X|ON)){12}", line):
+#			print line
 			n_times += 1
-			statuses = line.split("|")
+			statuses = line.split()[1:]
 			for i in range(len(statuses)):
 				if statuses[i].strip() == "ON":
-					active.append(i - 1)
-	if n_times < 1:
-		print ">> ERROR: No \"init\" was called on the link."
-	if n_times > 1:
-		print ">> ERROR: Hm, \"init\" was called on the link multiple times, so the active channel list might be unreliable."
-	return active
+					active.append( 12 * ((n_times - 1) % 2) + i )
+	if n_times < 2:
+		print ">> ERROR: No correct \"status\" was called on the link."
+	elif n_times > 2:
+		print ">> ERROR: Hm, \"status\" was called on the link multiple times, so the active channel list might be unreliable. (n_times = {0})".format(n_times)
+	if (n_times % 2 != 0):
+		print ">> ERROR: Uh, there were an odd number of \"status\" lines."
+	return list(set(active))
 
 
 # Parse uHTR data
@@ -78,23 +82,27 @@ for line in raw_data:
 data["channels"] = active_channels(raw_output)
 
 # Functions to analyze this "data" dictionary
-def check_cid(d):		# Check if the CIDs are rotating.
+def check_cid(d):		# Check if the CIDs are rotating. This function returns (1) if the CIDs are rotating correctly, (0) if they aren't, and (-1) if there is no CID data in the input.
 	start = -1
 	n_error = 0
-	if ( len(set(d["cid"][0])) == 1 ):		# Check that all CIDs start at the same value.
-		start = list(set(d["cid"][0]))[0]
-	if start != -1:
-#		print start
-		for i in range(4):		# Loop over each channel.
-			for j in range(len(d["cid"])):		# Loop over the BXs.
-				if ( (j + start) % 4  != d["cid"][j][i] ):
-					n_error += 1
-#					print j
-#					print d["cid"][j]
-	else:
-		">> ERROR: The CIDs don't all start at the same value!"
 	check = 0
-	if ( n_error == 0 ):
+	try:
+		if ( len(set(d["cid"][0])) == 1 ):		# Check that all CIDs start at the same value.
+			start = list(set(d["cid"][0]))[0]
+		if start != -1:
+	#		print start
+			for i in range(4):		# Loop over each channel.
+				for j in range(len(d["cid"])):		# Loop over the BXs.
+					if ( (j + start) % 4  != d["cid"][j][i] ):
+						n_error += 1
+	#					print j
+	#					print d["cid"][j]
+		else:
+			">> ERROR: The CIDs don't all start at the same value!"
+	except Exception as ex:
+#		print ex
+		check = -1
+	if ( n_error == 0 and check != -1 ):
 		check = 1
 	return check
 
@@ -104,11 +112,12 @@ def check_cid(d):		# Check if the CIDs are rotating.
 #print data["adc"]
 #print data["tdc_le"]
 #print data["tdc_te"]
-channels = active_channels(raw_output)
-print ">> The activated channels are {0}.".format(channels)
-print ">> You're checking {0} bunch crossings.".format(len(data["cid"]))
+print ">> The activated channels are {0}.".format(data["channels"])
+print ">> You're checking {0} bunch crossings. (You asked to check {1}, n_samples = {2})".format(len(data["cid"]), n_bx, n_samples)
 z_cid = check_cid(data)
-if z_cid:
+if (z_cid == 1):
 	print ">> The CIDs are rotating correctly."
-else:
+elif (z_cid == 0):
 	print ">> DANGER: The CIDs are not rotating correctly."
+elif (z_cid == -1):
+	print ">> ERROR: There was no CID data recorded."
