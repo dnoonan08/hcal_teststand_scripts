f596360a537719a8a7be15bbee05bb77954c39f2
Tote Hughes <elhughes@srv-e1x02-07-01.cms904>
2015-02-17 13:17:06 +0100

    150217: I added GLIB and Bridge FPGA informaion to the versions.py script.

--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
-Documentation should go here.
+# Overview
+This is a collection of scripts to use on the HCAL teststands.
 
+# Documentation
 Here is a short summary of what the different scripts do:
 
-* `versions.py`: Displays software and firmware versions of the different teststand components. This is very incomplete code.
+* `versions.py`: Displays software and firmware versions of the different teststand components. So far it displays information for the AMC13, GLIB, and Bridge FPGA.
 * `qie_card_valid.py`: Determines if a QIE card is operating correctly. This is ver incomplete code; so far it just tests that the card's CIDs are rotating. 
--- versions.py
+++ versions.py
@@ -28,9 +28,9 @@ def get_amc_info():
 		"log":			log,
 	}
 
-def print_amc():
+def print_amc_info():
 	amc_info = get_amc_info()
-	print "* AMC13 =================================="
+	print "* AMC13  ================================="
 	if (amc_info["sn"] == -1):
 		print "\tERROR: There was a problem fetching the AMC13 information."
 		print "\tThe log is below:\n++++++++++++++ LOG ++++++++++++++++++\n{0}\n+++++++++++++ /LOG ++++++++++++++++++".format(amc_info["log"])
@@ -40,37 +40,90 @@ def print_amc():
 		print "\tSW version: {0}".format(amc_info["version_sw"])
 #		print "\tLog: {0}".format(amc_info["log"])
 
-def get_glib_info():
+def ngccm_command(cmd):
 	port = 4242
-	commands = 'get fec1-user_firmware_dd\nget fec1-user_firmware_mm\nget fec1-user_firmware_yy\nquit'
-	raw_output = Popen(['printf "{0}" | ngccm -z -c -p {1}'.format(commands, port)], shell = True, stdout = PIPE, stderr = PIPE).communicate()		# This puts the output of the command into a list called "raw_output" the first element of the list is stdout, the second is stderr.
-	print raw_output[0]
-	print raw_output[1]
-#	version_sw = 0
-#	version_fw = []
-#	sn = -1
-#	log = ""
-#	try:
-#		match = search("Using AMC13 software ver:\s*(\d+)", raw_output[1])		# For some reason the software verion number is printed on stderr...
-#		version_sw = int(match.group(1))
-#	except Exception as ex:
-#		log += "Trying to find AMC13 software version number resulted in: {0}\n".format(ex)
-#	try:
-#		match = search("SN:\s+(\d+)\s+T1v:\s+(\d+)\s+T2v:\s+(\d+)", raw_output[0].strip())		# Look in stdout for the firmware version.
-#		sn = int(match.group(1))
-#		version_fw.append(int(match.group(2)))
-#		version_fw.append(int(match.group(3)))
-#	except Exception as ex:
-#		log += "Trying to find AMC13 firmware version and SN resulted in: {0}\n".format(ex)
-#	if (log == ""):
-#		log = "Empty"
-#	return {
-#		"sn":			sn,
-#		"version_sw":	version_sw,
-#		"version_fw":	version_fw,
-#		"log":			log,
-#	}
+	raw_output = ""
+	if isinstance(cmd, str):
+		raw_output_temp = Popen(['printf "{0}" | ngccm -z -c -p {1}'.format(cmd + "\nquit", port)], shell = True, stdout = PIPE, stderr = PIPE).communicate()		# This puts the output of the command into a list called "raw_output_temp" the first element of the list is stdout, the second is stderr.
+		raw_output += raw_output_temp[0] #+ raw_output_temp[1]
+	elif isinstance(cmd, list):
+		for c in cmd:
+			raw_output_temp = Popen(['printf "{0}" | ngccm -z -c -p {1}'.format(c + "\nquit", port)], shell = True, stdout = PIPE, stderr = PIPE).communicate()		# This puts the output of the command into a list called "raw_output_temp" the first element of the list is stdout, the second is stderr.
+			raw_output += raw_output_temp[0] + raw_output_temp[1]
+	return raw_output
+
+def get_glib_info():
+	data = {
+		"version_fw_d": ['get fec1-user_firmware_dd', 0],
+		"version_fw_m": ['get fec1-user_firmware_mm', 0],
+		"version_fw_y": ['get fec1-user_firmware_yy', 0],
+	}
+	log = ""
+	raw_output = ngccm_command([data[info][0] for info in data.keys()])
+	for info in data.keys():
+		try:
+			match = search("{0} # ((0x)?[0-9a-f]+)".format(data[info][0]), raw_output)		# For some reason the software verion number is printed on stderr...
+			data[info][1] = int(match.group(1), 16)
+		except Exception as ex:
+			log += 'Trying to find the result of "{0}" resulted in: {1}\n'.format(command, ex)
+	if (log == ""):
+		log = "Empty"
+	version_fw = "{0:02d}{1:02d}{2:02d}".format(data["version_fw_y"][1], data["version_fw_m"][1], data["version_fw_d"][1])
+	return {
+		"version_fw_d":	data["version_fw_d"][1],
+		"version_fw_m":	data["version_fw_m"][1],
+		"version_fw_y":	data["version_fw_y"][1],
+		"version_fw":	version_fw,
+		"log":			log,
+	}
+
+def print_glib_info():
+	glib_info = get_glib_info()
+	print "* GLIB   ================================="
+	if (glib_info["version_fw"] == "000000"):
+		print "\tERROR: There was a problem fetching the GLIB information."
+		print "\tThe log is below:\n++++++++++++++ LOG ++++++++++++++++++\n{0}\n+++++++++++++ /LOG ++++++++++++++++++".format(glib_info["log"])
+	else:
+		print "\tFW version: {0}".format(glib_info["version_fw"])
+
+def get_bridge_info():
+	data = {
+		"version_fw_major": ['get HF1-2-B_FIRMVERSION_MAJOR', 0],
+		"version_fw_minor": ['get HF1-2-B_FIRMVERSION_MINOR', 0],
+		"version_fw_svn": ['get HF1-2-B_FIRMVERSION_SVN', 0],
+	}
+	log = ""
+	raw_output = ngccm_command([data[info][0] for info in data.keys()])
+	for info in data.keys():
+		try:
+			match = search("{0} # ((0x)?[0-9a-f]+)".format(data[info][0]), raw_output)		# For some reason the software verion number is printed on stderr...
+			data[info][1] = int(match.group(1), 16)
+		except Exception as ex:
+			log += 'Trying to find the result of "{0}" resulted in: {1}\n'.format(command, ex)
+	if (log == ""):
+		log = "Empty"
+	version_fw = "{0:02d}.{1:02d}.{2:04d}".format(data["version_fw_major"][1], data["version_fw_minor"][1], data["version_fw_svn"][1])
+	return {
+		"version_fw_major":	data["version_fw_major"][1],
+		"version_fw_minor":	data["version_fw_minor"][1],
+		"version_fw_svn":	data["version_fw_svn"][1],
+		"version_fw":	version_fw,
+		"log":			log,
+	}
+
+def print_bridge_info():
+	bridge_info = get_bridge_info()
+	print "* BRIDGE ================================="
+	if (bridge_info["version_fw"] == "00.00.0000"):
+		print "\tERROR: There was a problem fetching the BRIDGE information."
+		print "\tThe log is below:\n++++++++++++++ LOG ++++++++++++++++++\n{0}\n+++++++++++++ /LOG ++++++++++++++++++".format(bridge_info["log"])
+	else:
+		print "\tFW version: {0}".format(bridge_info["version_fw"])
 
 if __name__ == "__main__":
-	print_amc()
-#	get_glib_info()
+#	print "=========================================="
+	print_amc_info()
+	print_glib_info()
+	print_bridge_info()
+#	print "=========================================="
+#	get_bridge_info()
