60cd6e6ec280a9b7c88a6b77b199725b4d224968
Tote Hughes <elhughes@srv-e1x02-07-01.cms904>
2015-05-26 15:38:13 +0200

    150526: Added loop over all crates and slots, added more information in the summary.

--- BER_test_of_SlowControl.py
+++ BER_test_of_SlowControl.py
@@ -45,52 +45,87 @@ if __name__ == "__main__":
 	# Set up:
 	ts = teststand(name)		# Initialize a teststand object. This object stores the teststand configuration and has a number of useful methods.
 	print ">> Running BER test on teststand {0}.".format(name)
-	print ">> Processing {0} cycle(s) ...".format(n)
 	
 	# Cycle n times:
-	n_cycles = 0
-	n_errors = 0
-	errors = []
-	for j in range(n):
-		# Do a cycle:
-		if v: print "==== Cycle {0} ============================================".format(j + 1)
-		for i in range(1, 33):
-			# Construct test string:
-			b = "0" * (32 - i)
-			b += "1"
-			b += "0" * (i - 1)
-			if v: print "Binary string to write: {0}".format(b)
-		
-			# Convert test string to hex:
-			h_write = "{0:#010x}".format(int(b, 2))
-			if v: print "Hex string to write: {0}".format(h_write)
-		
-			# Write, then read:
-			script = [
-				"put HF{0}-{1}-B_SCRATCH {2}".format(crate, slot, h_write),
-				"get HF{0}-{1}-B_SCRATCH".format(crate, slot)
-			]
-			output = ngccm.send_commands_parsed(ts.ngccm_port, script)["output"]
-			h_read = "{0:#010x}".format(int(output[1]["result"], 16))
-			for cmd in output:
-				if v: print "<< Command: {0} -> {1}".format(cmd["cmd"], cmd["result"])
-			if v: print "Hex string read: {0}".format(h_read)
-		
-			# Check for error:
-			if h_write != h_read:
-				n_errors += 1
-				errors.append([h_write, h_read])
-				if v: print "ERROR: The read value of {0} didn't match the written value of {1}.".format(h_read, h_write)
+	n_cycles = {}
+	n_errors = {}
+	n_errors_read = {}
+	errors = {}
+	for crate, slots in ts.fe.iteritems():
+		for slot in slots:
+			# Set up variables:
+			n_cycles[crate, slot] = 0
+			errors[crate, slot] = []
+			n_errors[crate, slot] = 0
+			n_errors_read[crate, slot] = 0
 			
-			if v: print ""
-		n_cycles += 1
+			# Start cycle
+			print "\n>> Processing {0} cycle(s) for crate {1}, slot {2} ...".format(n, crate, slot)
+			for j in range(n):
+				# Do a cycle:
+				if v: print "==== Cycle {0} ============================================".format(j + 1)
+				script = []
+				h_writes = []
+				bs = []
+				for i in range(1, 33):
+					# Construct test string:
+					b = "0" * (32 - i)
+					b += "1"
+					b += "0" * (i - 1)
+					bs.append(b)
+					
+					# Convert test string to hex:
+					h_write = "{0:#010x}".format(int(b, 2))
+					
+					# Construct script:
+					script.append("put HF{0}-{1}-B_SCRATCH {2}".format(crate, slot, h_write))
+					script.append("get HF{0}-{1}-B_SCRATCH".format(crate, slot))
+					h_writes.append(h_write)
+				
+				# Perform writting and reading:
+				print ">> Talking to ngccm server ..."
+				output = ngccm.send_commands_parsed(ts.ngccm_port, script)["output"]
+				
+				# Deal with results:
+				for i in range(1, 33):
+					h_write = h_writes[i - 1]
+					if v: print "Binary string to write: {0}".format(bs[i - 1])
+					if v: print "Hex string to write: {0}".format(h_write)
+					try:
+						h_read = "{0:#010x}".format(int(output[i*2 - 1]["result"], 16))
+					except:
+						h_read = False
+					for cmd in output[2*i-2:2*i]:
+						if v: print "<< Command: {0} -> {1}".format(cmd["cmd"], cmd["result"])
+					if v: print "Hex string read: {0}".format(h_read)
+					
+					# Check for error:
+					if h_read and h_write != h_read:
+						n_errors[crate, slot] += 1
+						errors[crate, slot].append([h_write, h_read])
+						if v: print "ERROR: The read value of {0} didn't match the written value of {1}.".format(h_read, h_write)
+					elif not h_read:
+						n_errors_read[crate, slot] += 1
+						if v: print "ERROR: The written value of {0} could not be read back.".format(h_write)
+					
+					if v: print ""
+				n_cycles[crate, slot] += 1
 	
-	# Print summary:
+	# Summarize:
 	print "===== SUMMARY ====="
-	print "Total number of cycles: {0}".format(n_cycles)
-	print "! Number of mismatch errors: {0}".format(n_errors)
-	if n_errors:
-		print "! Below is some information about these errors:"
-		for i in range(len(errors)):
-			print "{0}: string written = {1}, string read = {2}".format(i + 1, errors[i][0], errors[i][1])
+	print "Results for the {0} teststand:".format(ts.name)
+	for crate, slots in ts.fe.iteritems():
+		for slot in slots:
+			# Set up variables:
+			s = "{0:2d}{1:2d}".format(crate, slot)
+			
+			# Print summary:
+			print "\n= QIE card in crate {0}, slot {1}".format(crate, slot)
+			print "Total number of cycles: {0}".format(n_cycles[crate, slot])
+			print "! Number of mismatch errors: {0}".format(n_errors[crate, slot])
+			if n_errors[crate, slot]:
+				print "! Below is some information about these errors:"
+				for i in range(len(errors[crate, slot])):
+					print "{0}: string written = {1}, string read = {2}".format(i + 1, errors[crate, slot][i][0], errors[crate, slot][i][1])
+			if n_errors_read[crate, slot] > 0 : print "! Number of software read errors: {0}".format(n_errors_read[crate, slot])
 # /MAIN
