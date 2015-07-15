30664dce12f5ca41a1ec6c06040384848aef672e
whitbeck.andrew@gmail.com <whitbeck@srv-e1x02-07-01.cms904>
2015-07-15 12:08:43 +0200

    adding code for testing QIE registers

---
+++ QIEregisterTest.py
@@ -0,0 +1,79 @@
+from hcal_teststand.register import *
+from hcal_teststand.hcal_teststand import teststand
+import sys
+import random
+
+def getRandomValue( ) : 
+
+	### 
+	# the 47 should not be hardcoded and should instead
+	# be taken from register.size.  However, this needs to 
+	# be done carefully so taht the bits are divided up
+	# into 24 bit chunks correctly...
+	###
+	randomInt = random.randint(0,1)	
+	for i in range( 47 ) :
+		if random.randint(0,1) : 
+			randomInt = ( randomInt | 1 ) << 1
+		else :
+			randomInt = randomInt << 1
+			
+	return str(hex(randomInt))
+		
+def testRandomValue( register ):
+
+	randomValue = getRandomValue()
+
+	testValues = [ randomValue[0:8] , 
+		       '0x'+randomValue[8:16]
+		       ]
+
+	#print "testing:",testValues
+        value = ''
+        register.write(testValues[1]+" "+testValues[0])
+        value = register.read()
+
+	if value.find("ERROR") != -1 : 
+		print "ERROR in results"
+		return False
+
+        values = value.split("'")[1].split()
+	#print "result:",values
+	if values == testValues : 
+		return True
+	else : 
+		return False 
+
+
+# MAIN:
+if __name__ == "__main__":
+	name = ""
+	if len(sys.argv) == 1:
+		name = "904"
+	elif len(sys.argv) == 2:
+		name = sys.argv[1]
+	else:
+		name = "904"
+	ts = teststand(name)		# Initialize a teststand object. This object stores the teststand configuration and has a number of useful methods.
+
+	result = [0]*24
+	registers = []
+	for q in range(24) : 
+		registers.append( register(ts,"QIE{0}".format(q+1),48) )
+
+	for i in range( 5 ) :
+		#print "test",i
+		for r in registers :
+			#print r.name
+		        value = getRandomValue()
+			values = [ value[0:8] , '0x'+value[8:16] ]
+			r.addTestToCache( values[0]+" "+values[1] )
+			#if not testRandomValue( r ) :
+			#	result[q] = result[q] + 1
+		#print "ERRORS: ",result
+
+	for r in registers : 
+		r.testCache()
+
+# /MAIN
+
---
+++ hcal_teststand/register.py
@@ -0,0 +1,111 @@
+##############################################
+# Register.py - 
+#    class to implement simple read/write actions
+#
+# Author: A. Whitbeck - July 11, 2015
+##############################################
+
+import ngccm
+from hcal_teststand import *
+
+class register : 
+    
+    # CONSTRUCTOR
+    def __init__( self , 
+                  ts        ,
+                  name = "" ,  # currently, name should corerspond to the name used by the ngccm, ignoring HF<crate>-<slot>-  
+                  size = 1     # size should be number of bits
+                  ):
+
+        self.verbosity = 0 
+
+        #output = ngccm.send_commands_parsed(ts, ["ls"] )["output"]
+        #if self.verbosity >= 1 :
+        #    print "REGISTER CONSTRUCTOR --"
+        #    print output
+
+        self.ts        = ts
+        self.name      = name
+        self.size      = size
+        self.commandCache = []
+
+    def setVerbosity( self , verb ) :
+        self.verbosity = verb
+
+    def testCache( self ) :
+        
+        errors = 0 
+        executeErrors = 0 
+        
+        output = ngccm.send_commands_parsed( self.ts , self.commandCache )["output"]
+
+        if self.verbosity >= 1 :
+            print output
+        
+        if len( output ) % 2 != 0 : 
+            print "ERROR, register::testCach() - the wrong number of commands were executed"
+            return -999 
+        for i in range( len( output ) / 2 ) :
+            if output[i*2]["cmd"].find("put ") == -1 :
+                print "ERROR, register::testCach() - commands executed in wrong order??"
+                return -999 
+            else :
+                value = output[i*2]["cmd"].split()[2:]
+
+            valueNum = []
+            for v in value :
+                valueNum.append( int(v,16) )
+
+            if output[i*2]["result"] != "OK" : 
+                executeErrors = executeErrors + 1
+                continue
+
+            if output[i*2+1]["cmd"].find("get ") == -1 :
+                print "ERROR, register::testCach() - commands executed in wrong order??"
+                return -999 
+            else : 
+                check = output[i*2+1]["result"].split("'")[1].split()
+
+            checkNum = []
+            for c in check[::-1] :
+                checkNum.append( int(c,16) )
+
+            ### valueNum and checkNum are compared so that
+            ### leading zeroes will be conveniently ignored
+            if self.verbosity >= 1 :
+                print "value",value,"check",check
+            if valueNum != checkNum : 
+                errors = errors + 1
+
+        print "--------------------------------"
+        print "REGISTER:",self.name
+        print "--------------------------------"
+        print "errors:",errors
+        print "execution errors:",executeErrors
+                
+    def addTestToCache( self, value ) :
+
+        self.commandCache.append( "put HF1-10-{0} {1}".format( self.name , value ) )
+        self.commandCache.append( "get HF1-10-{0}".format( self.name , value ) )
+        
+    def read( self ) :
+        
+        output = ngccm.send_commands_parsed( self.ts, ["get HF1-10-{0}".format(self.name)] )["output"][0]["result"]
+        if self.verbosity >= 1 : 
+            print "REGISTER::READ() --"
+            print output
+
+        return output
+
+    def write( self , value = '') :
+        
+        output = ngccm.send_commands_parsed(self.ts, ["put HF1-10-{0} {1}".format(self.name,value)] )["output"][0]["result"]
+        if self.verbosity >= 1 : 
+            print "REGISTER::WRITE() --"
+            print output
+        
+        if output.find("ERROR!") != -1 : 
+            return False
+        else : 
+            return True
+        
