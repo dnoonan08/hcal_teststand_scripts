2da155d8933b782b44e8bbe6dfbf907791b0bf2a
Yanchu Wang <yanchu@cern.ch>
2015-07-17 13:16:12 +0200

    format changed

--- igloospy.py
+++ igloospy.py
@@ -14,17 +14,20 @@ if __name__=="__main__":
 	else:
 		name = "904"
 	ts = teststand(name)		# Initialize a teststand object. This object stores the teststand configuration and has a number of useful methods.
-        fl=open('iglooout.txt','w')
 	fldec=open('igloodec.txt','w')
+	sepcut='---------------------------------------------------------------------------------------------------------------\n'
+	ostr=sepcut+'channel(T/B)\t'
+	for i in range(12):
+		ostr+='{0}/{1}\t'.format(i+1,i+13)
+	ostr+='\n'
+	fldec.write(ostr)
         command1=['put HF{0}-{1}-iTop_CntrReg 0x2'.format(crate,slot),'put HF{0}-{1}-iTop_CntrReg 0x0'.format(crate,slot)]
         command2=['put HF{0}-{1}-iBot_CntrReg 0x2'.format(crate,slot),'put HF{0}-{1}-iBot_CntrReg 0x0'.format(crate,slot)]
         command3=['get HF{0}-{1}-iTop_StatusReg'.format(crate,slot),'get HF{0}-{1}-iTop_inputSpy'.format(crate,slot)]*512
         command4=['get HF{0}-{1}-iBot_StatusReg'.format(crate,slot),'get HF{0}-{1}-iBot_inputSpy'.format(crate,slot)]*512
         command1.extend(command2)
         command1.extend(command3)
-	command1.append('get HF{0}-{1}-iTop_StatusReg'.format(crate,slot))
         command1.extend(command4)
-	command1.append('get HF{0}-{1}-iBot_StatusReg'.format(crate,slot))
         outp=ngccm.send_commands_parsed(ts,command1)['output']
         for opt in outp:
             if opt['cmd'][:3] == 'put':
@@ -33,13 +36,22 @@ if __name__=="__main__":
                 else:
                     print 'ERROR'
                     exit()
-	    ostr=opt['cmd']+': \t'
-            fl.write(ostr+opt['result']+'\n')
+	    ostr=''
 	    if opt['cmd'][-3:] == 'Reg':
 		    deco=bin(int(opt['result'][:-5],16))
-		    ostr+='Data_left:{0},\tEmpty:{1},\tFull:{2}\n'.format(int(deco[:-2],2),deco[-2],deco[-1])
+		    ostr+=sepcut
+		    lth=deco[:-2]
+		    if lth=='0b':
+			    lth+='0'
+		    ostr+='(Data_left:{0},  Empty:{1},  Full:{2})\n'.format(int(lth,2),deco[-2],deco[-1])
 	    if opt['cmd'][-3:] == 'Spy':
-		    ostr+='\n'
+		    ostr+=''
+		    if opt['cmd'][-12:-9] == 'Top':
+			    ostrid='capid(T)\t'
+		    else:
+			    ostrid='capid(B)\t'
+		    ostradc='ADC\t\t'
+		    ostrtdc='TDC\t\t'
 		    bindecos=[]
 		    hexdecos=opt['result'].strip("'").split()[1:]
 		    for deco in hexdecos:
@@ -54,8 +66,11 @@ if __name__=="__main__":
 			    for i in tdcorder:
 				    tdc+=deco[15-i]
 			    capid=deco[0]+deco[8]
-			    ostr+='\t capid:{0},\tADC:{1},\tTDC:{2}\n'.format(int(capid,2),int(adc,2),int(tdc,2))
+			    ostrid+='{0}\t'.format(int(capid,2))
+			    ostradc+='{0}\t'.format(int(adc,2))
+			    ostrtdc+='{0}\t'.format(int(tdc,2))
+		    ostr+='{0}\n{1}\n{2}\n'.format(ostrid,ostradc,ostrtdc)
 	    fldec.write(ostr)
-	fl.close()
+	fldec.write(sepcut)
 	fldec.close()
 
