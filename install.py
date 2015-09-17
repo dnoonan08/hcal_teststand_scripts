7db599a2b7fccfbdb5df46fbbabf4b7bc6172c7f
Tote Hughes <tote@physics.rutgers.edu>
2015-09-17 13:33:07 +0200

    150917: Fixed a bug in install.py

--- install.py
+++ install.py
@@ -8,12 +8,13 @@
 ####################################################################
 
 from re import search, split
-from hcal_teststand_scripts import hcal_teststand
+from hcal_teststand import hcal_teststand
+from hcal_teststand import amc13
 
 # FUNCTIONS:
 def make_amc13_configs(f="teststands.txt"):		# Write configuration files for AMC13.
 	names = []
-	ts_info = parse_ts_configuration(f=f)
+	ts_info = hcal_teststand.parse_ts_configuration(f=f)
 	for name, info in ts_info.iteritems():
 		if "control_hub" in info:
 			ch = info["control_hub"]
@@ -21,8 +22,11 @@ def make_amc13_configs(f="teststands.txt"):		# Write configuration files for AMC
 			ch = "localhost"
 		if info:
 			string = '<?xml version="1.0" encoding="UTF-8"?>\n<connections>\n'
-			for i, ips in enumerate(info["amc13_ips"]):
-				string += '\t<connection id="{0}.T1" uri="chtcp-2.0://'.format(i) + ch + ':10203?target=' + ips[0] + ':50001" address_table="file:///opt/cactus/etc/amc13/AMC13XG_T1.xml" />\n\t<connection id="{0}.T2" uri="chtcp-2.0://'.format(i) + ch + ':10203?target=' + ips[1] + ':50001" address_table="file:///opt/cactus/etc/amc13/AMC13XG_T2.xml" />\n'
+			amc13s = [ips for ips in info["amc13_ips"] if ips]
+			for i, ips in enumerate(amc13s):
+				string += '\t<!-- AMC13 with SN = {0} -->\n'.format(amc13.sn_from_ip(ips[0]))
+				string += '\t<connection id="{0}T1" uri="chtcp-2.0://'.format(str(i) + "." if len(amc13s) > 1 else "") + ch + ':10203?target=' + ips[0] + ':50001" address_table="file:///opt/cactus/etc/amc13/AMC13XG_T1.xml" />\n'
+				string += '\t<connection id="{0}T2" uri="chtcp-2.0://'.format(str(i) + "." if len(amc13s) > 1 else "") + ch + ':10203?target=' + ips[1] + ':50001" address_table="file:///opt/cactus/etc/amc13/AMC13XG_T2.xml" />\n'
 			string += '</connections>'
 			try:
 				out = open("configuration/amc13_" + name + "_config.xml", "w")
@@ -36,7 +40,7 @@ def make_amc13_configs(f="teststands.txt"):		# Write configuration files for AMC
 
 def make_setup_scripts(f):		# Write the set up scripts.
 	names = []
-	ts_info = parse_ts_configuration(f)
+	ts_info = hcal_teststand.parse_ts_configuration(f)
 	for name, info in ts_info.iteritems():
 		string = ""
 		if info:
