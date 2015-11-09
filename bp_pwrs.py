406a18c0260d364717750451dee1a552a9e43871
Ohannes Koseyan <okoseyan@srv-e1x02-07-01.cms904>
2015-11-09 22:32:14 +0100

    Script takes the FC7 slot as the first argument.

--- bp_pwrs.py
+++ bp_pwrs.py
@@ -1,11 +1,12 @@
+import sys
 from hcal_teststand import ngfec
 from hcal_teststand.hcal_teststand import teststand
 
 ts = teststand("904at")		# Create teststand object for ngfec.send_commands()
 
-print 'Enter the crate number to be reset:'
-cr = int(raw_input())
+cr = sys.argv[1]
 
 commands = ['put HF{0}-bkp_pwr_enable 0'.format(cr), 'put HF{0}-bkp_pwr_enable 1'.format(cr), 'put HF{0}-bkp_reset 0'.format(cr), 'put HF{0}-bkp_reset 1'.format(cr), 'put HF{0}-bkp_reset 0'.format(cr)]
-
-ngfec.send_commands(ts, cmds = commands, script = False)
+results = ngfec.send_commands(ts, cmds = commands, script = False)
+for result in results:
+	print result['cmd'][4:] + ': [' + result['result'] + ']'
