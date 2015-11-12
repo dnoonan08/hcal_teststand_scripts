f79e6d4ac6314bf2db7ba19015cb5dbdb43cc3ab
Ohannes Koseyan <okoseyan@srv-e1x02-07-01.cms904>
2015-11-12 12:29:12 +0100

    A script used by ngfec.py to kill old ngccm processes.

--- killccm.sh
+++ killccm.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
-i=0
 #processes="$(ps o pid=,cmd= -C ngccm)"
-processes="$(ps -e | grep ngccm)"
-for process in $processes
+pids="$(pgrep -u `whoami` ngccm)"
+for pid in $pids
 do
-#echo $process
-if ! (( $i % 4 ))
-then
-t="$(ps -p $process -o etime=)"
-t="$(echo $t | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')"
+t="$(ps -p $pid -o etime=)"
+t="$(echo $t | awk -F: '{ print ($1 * 60) + $2 }')"
 if [ "$t" -gt 3600 ]
 then
-#echo $(( $i / 4 ))':' $process '->' $t
-kill -9 $process &> /dev/null
+kill -9 $pid &> /dev/null
 fi
-fi
-i=$(( $i + 1 ))
 done
