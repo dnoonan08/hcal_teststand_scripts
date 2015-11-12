6c8bc7f8f7f3014166cebbb2aaea2181c24a787e
Ohannes Koseyan <okoseyan@srv-e1x02-07-01.cms904>
2015-11-12 12:56:52 +0100

    A script used by ngfec.py to kill ngccm processesolder than 3600 seconds (1 hour).

--- killccm.sh
+++ killccm.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-#processes="$(ps o pid=,cmd= -C ngccm)"
 pids="$(pgrep -u `whoami` ngccm)"
 for pid in $pids
 do
 t="$(ps -p $pid -o etime=)"
-t="$(echo $t | awk -F: '{ print ($1 * 60) + $2 }')"
+t="$(echo $t | tr '-' ':' | awk -F: '{ total=0; m=1; } { for (i=0; i < NF; i++) {total += $(NF-i)*m; m *= i >= 2 ? 24 : 60 }} {print total}')"
 if [ "$t" -gt 3600 ]
 then
 kill -9 $pid &> /dev/null
+#echo $pid: $t '->' Kill
+#else
+#echo $pid: $t '->' Mercy
 fi
 done
